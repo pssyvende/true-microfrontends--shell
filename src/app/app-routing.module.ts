798efff3f18d1dbafa98147ec64524f62d8a28a4
Paulina Sikacka <paulina.sikacka@convista.com>
2024-08-09 12:10:06 +0200

    4. configure routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { loadRemoteModule, LoadRemoteModuleOptions } from '@angular-architects/module-federation';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'mfe1',
+    loadChildren: () => {
+      return loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Module'
+      } as LoadRemoteModuleOptions)
+      .then(m => m.FeatureAModule)
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
